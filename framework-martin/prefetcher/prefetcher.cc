40cbc988804592d92bd32de3ae2c6803d238ac41
Martin Jensen <martin.jensen.91@gmail.com>
2016-03-07 10:06:14 +0100

    RPT complete

--- framework-martin/prefetcher/prefetcher.cc
+++ framework-martin/prefetcher/prefetcher.cc
@@ -2,10 +2,16 @@
 
 #define TABLE_SIZE 128
 
+#define STATE_INIT 0
+#define STATE_TRANSIENT 1
+#define STATE_STEADY 2
+#define STATE_NO_PREDICTION 3
+
 typedef struct {
     Addr tag;
     Addr prevAddr;
     Addr stride;
+    uint8_t state;
 } RPTEntry;
 
 RPTEntry table[TABLE_SIZE];
@@ -21,14 +27,60 @@ void prefetch_access(AccessStat stat)
 	table[entry].prevAddr = stat.mem_addr;
 	table[entry].stride = 0;
 	table[entry].tag = stat.pc;
+	table[entry].state = STATE_INIT;
+
     } else {
-	if ((stat.mem_addr - table[entry].prevAddr) == table[entry].stride) {
-	    if (!in_cache(stat.mem_addr + table[entry].stride)) {
-		issue_prefetch(stat.mem_addr + table[entry].stride);
+	int correct = (stat.mem_addr - table[entry].prevAddr) == table[entry].stride;
+
+	switch(table[entry].state) {
+	case STATE_INIT:
+	    if (!correct) {
+		table[entry].stride = stat.mem_addr - table[entry].prevAddr;
+		table[entry].prevAddr = stat.mem_addr;
+		table[entry].state = STATE_TRANSIENT;
+	    } else {
+		table[entry].prevAddr = stat.mem_addr;
+		table[entry].state = STATE_STEADY;
 	    }
+	    break;
+
+	case STATE_TRANSIENT:
+	    if (!correct) {
+		table[entry].stride = stat.mem_addr - table[entry].prevAddr;
+		table[entry].prevAddr = stat.mem_addr;
+		table[entry].state = STATE_NO_PREDICTION;
+	    } else {
+		table[entry].prevAddr = stat.mem_addr;
+		table[entry].state = STATE_STEADY;
+	    }
+	    break;
+
+	case STATE_NO_PREDICTION:
+	    if (!correct) {
+		table[entry].stride = stat.mem_addr - table[entry].prevAddr;
+		table[entry].prevAddr = stat.mem_addr;
+	    } else {
+		table[entry].prevAddr = stat.mem_addr;
+		table[entry].state = STATE_TRANSIENT;
+	    }
+	    break;
+
+	case STATE_STEADY:
+	    if (!correct) {
+		table[entry].prevAddr = stat.mem_addr;
+		table[entry].state = STATE_INIT;
+	    } else {
+		table[entry].prevAddr = stat.mem_addr;
+	    }
+	    break;
 	}
-	table[entry].stride = stat.mem_addr - table[entry].prevAddr;
-	table[entry].prevAddr = stat.mem_addr;
+
+	int prefetchAddr = table[entry].prevAddr + table[entry].stride;
+	if ( table[entry].state == STATE_STEADY &&
+	     correct &&
+	     !in_cache(prefetchAddr) ) {
+	    issue_prefetch( prefetchAddr );
+	}
     }
 }
 
